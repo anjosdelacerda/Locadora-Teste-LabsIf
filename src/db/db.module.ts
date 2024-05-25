70217b7c0550c3d44156f53522fdce51e1e45c88
anjosdelacerda <anjosdelacerda@gmail.com>
2024-05-25 16:45:09 -0300

    :feat: adicionando variáveis de deploy + confugrações de deploy

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -11,11 +11,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
+        host:
+          configService.get<string>('NODE_ENV') === 'local'
+            ? configService.get<string>('DB_HOST')
+            : configService.get<string>('POSTGRES_DEPLOY_HOST'),
+        port:
+          configService.get<string>('NODE_ENV') === 'local'
+            ? +configService.get<number>('DB_PORT')
+            : +configService.get<string>('POSTGRES_DEPLOY_PORT'),
+        username:
+          configService.get<string>('NODE_ENV') === 'local'
+            ? configService.get<string>('DB_USERNAME')
+            : configService.get<string>('POSTGRES_DEPLOY_USER'),
+        password:
+          configService.get<string>('NODE_ENV') === 'local'
+            ? configService.get<string>('DB_PASSWORD')
+            : configService.get<string>('POSTGRES_DEPLOY_PASSWORD'),
+        database:
+          configService.get<string>('NODE_ENV') === 'local'
+            ? configService.get<string>('DB_NAME')
+            : configService.get<string>('POSTGRES_DEPLOY_DB'),
         entities: [__dirname + '/entities/**'],
         migrations: [__dirname + '/migrations/*.ts'],
         autoLoadEntities: true,
--- src/db/typeOrm.migration-config.ts
+++ src/db/typeOrm.migration-config.ts
@@ -11,11 +11,26 @@ const configService = new ConfigService();
 
 const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT'),
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  database: configService.get<string>('DB_NAME'),
+  host:
+    configService.get<string>('NODE_ENV') === 'local'
+      ? configService.get<string>('DB_HOST')
+      : configService.get<string>('POSTGRES_DEPLOY_HOST'),
+  port:
+    configService.get<string>('NODE_ENV') === 'local'
+      ? +configService.get<number>('DB_PORT')
+      : +configService.get<string>('POSTGRES_DEPLOY_PORT'),
+  username:
+    configService.get<string>('NODE_ENV') === 'local'
+      ? configService.get<string>('DB_USERNAME')
+      : configService.get<string>('POSTGRES_DEPLOY_USER'),
+  password:
+    configService.get<string>('NODE_ENV') === 'local'
+      ? configService.get<string>('DB_PASSWORD')
+      : configService.get<string>('POSTGRES_DEPLOY_PASSWORD'),
+  database:
+    configService.get<string>('NODE_ENV') === 'local'
+      ? configService.get<string>('DB_NAME')
+      : configService.get<string>('POSTGRES_DEPLOY_DB'),
   entities: [UserEntity, CarEntity, RentEntity],
   migrations: [__dirname + '/migrations/*.ts'],
   synchronize: false,
